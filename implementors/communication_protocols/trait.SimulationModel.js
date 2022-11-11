492c9ac39c72b29af355d193e8cf9f7c7dfec872
germolinal <germolinal@users.noreply.github.com>
2022-11-11 10:11:59 +0000

    Deploying to gh-pages from @ SIMPLE-BuildingSimulation/air@86f52474780e36c72678fc073ef61fe12b1d95f2 🚀

--- implementors/communication_protocols/trait.SimulationModel.js
+++ implementors/communication_protocols/trait.SimulationModel.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["air"] = [{"text":"impl SimulationModel for <a class=\"struct\" href=\"air/model/struct.AirFlowModel.html\" title=\"struct air::model::AirFlowModel\">AirFlowModel</a>","synthetic":false,"types":["air::model::AirFlowModel"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"air":[["impl SimulationModel for <a class=\"struct\" href=\"air/model/struct.AirFlowModel.html\" title=\"struct air::model::AirFlowModel\">AirFlowModel</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["air"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"air/model/struct.AirFlowModel.html\" title=\"struct air::model::AirFlowModel\">AirFlowModel</a>","synthetic":true,"types":["air::model::AirFlowModel"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"air":[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"air/model/struct.AirFlowModel.html\" title=\"struct air::model::AirFlowModel\">AirFlowModel</a>",1,["air::model::AirFlowModel"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
